bdd2a6a10d39ea300cb4c133fe8feda30b1c3a63
ledinhdong2708 <ledinhdong2708@gmail.com>
2022-10-13 16:33:05 +0700

    change device

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 window.onscroll = function () { scollNavBar() };
 
+
 function scollNavBar() {
 
-    if(document.body.className == 'light-mode'){
+    if(document.body.className == 'dark-mode'){
         if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
             document.querySelector('.header').classList.add('nav-scoll')
             document.querySelectorAll('.nav-items').forEach((items) => {
@@ -34,21 +35,40 @@ function scollNavBar() {
 }
 
 function changeBackgroundColor() {
-    document.body.classList.toggle('light-mode')
+    document.body.classList.toggle('dark-mode')
     setColor()
+    setColorNavscoll()
 }
 
 
 function setColor(){
-    if(document.body.className == 'light-mode'){
+    if(document.body.className == 'dark-mode'){
         document.querySelectorAll('.nav-items').forEach((items) => {
             items.style.color = 'white'
         })
     }
     else{
         document.querySelectorAll('.nav-items').forEach((items) => {
-            items.style.color = 'white'
+            items.style.color = 'black'
         })
     }
 }
+
+function setColorNavscoll() {
+    var header = document.querySelector('.header')
+    if(header.className == 'header nav-scoll'){
+        if(document.body.className == 'dark-mode'){
+            document.querySelectorAll('.nav-items').forEach((items) => {
+                items.style.color = 'black'
+            })
+        }
+        else{
+            document.querySelectorAll('.nav-items').forEach((items) => {
+                items.style.color = 'white'
+            })
+        }
+    }
+}
+
+
 
